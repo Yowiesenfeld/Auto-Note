1bfded5306eab46c9880db905608729d87538d9c
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-09-28 16:13:13 -0400

    almost done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const fs = require('fs'); 
+const { v4: uuidv4 } = require('uuid');
 
 // middleware
 app.use(express.json());
@@ -19,7 +21,29 @@ app.get('/notes', (req, res) => {
   res.sendFile(__dirname + '/Develop/public/notes.html');
 });
 
+// API routes
+app.get('/api/notes', (req, res) => {
+  const notes = require('./db.json');
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+
+  const notes = require('./db.json');
+
+  newNote.id = /* Generate a unique ID here */;
+
+  notes.push(newNote);
+
+// updated notes array back to the db.json file
+  fs.writeFileSync('./db.json', JSON.stringify(notes));
+
+  // Respond created note
+  res.json(newNote);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
