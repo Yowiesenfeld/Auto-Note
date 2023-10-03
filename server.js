375f1f76167c7e8382327cefa1ace1c5589397e5
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-10-03 17:08:44 -0400

    fixed bugs

--- server.js
+++ server.js
@@ -14,36 +14,56 @@ app.use(express.static('public'));
 // routes here
 // HTML routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/Develop/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(__dirname + '/Develop/public/notes.html');
+  res.sendFile(__dirname + '/public/notes.html');
 });
 
 // API routes
 app.get('/api/notes', (req, res) => {
-  const notes = require('./db.json');
+  const notes = require('./db/db.json');
   res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
 
-  const notes = require('./db.json');
+  const notes = require('./db/db.json');
 
-  newNote.id = /* Generate a unique ID here */;
+  newNote.id = uuidv4();
 
   notes.push(newNote);
 
-// updated notes array back to the db.json file
-  fs.writeFileSync('./db.json', JSON.stringify(notes));
+// updated notes
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
 
   // Respond created note
   res.json(newNote);
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  const notes = require('./db/db.json');
+
+  // Find the index of the note
+  const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+  if (noteIndex !== -1) {
+    notes.splice(noteIndex, 1);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+
+    res.json({ message: 'Note deleted' });
+  } else {
+    res.status(404).json({ error: 'Note not found' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
